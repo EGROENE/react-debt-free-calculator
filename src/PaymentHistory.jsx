fe08a60f86a1b84e529f692a64df2a77f7e3c8b8
Ethan <ethangroene@gmail.com>
2023-02-25 23:50:20 -0600

    Display prev interest

--- src/PaymentHistory.jsx
+++ src/PaymentHistory.jsx
@@ -15,16 +15,18 @@ class PaymentHistory extends React.Component {
                         <p key={pmt.transactionNumber + 'paymentDate'}>Date of Payment: { pmt.paymentDate.toLocaleString() }</p>
                         <p key={pmt.transactionNumber + 'prevPrincipal'}>Previous Principal: ${ pmt.prevPrincipal.toLocaleString(undefined,{minimumFractionDigits: 2,
                         maximumFractionDigits: 2}) }</p>
+                        <p key={pmt.transactionNumber + 'prevInterestOwed'}>Previous Interest: ${ pmt.prevInterestOwed.toLocaleString(undefined,{minimumFractionDigits: 2,
+                        maximumFractionDigits: 2}) }</p>
                         <p key={pmt.transactionNumber + 'prevBalance'}>Previous Balance: ${ pmt.prevBalance.toLocaleString(undefined,{minimumFractionDigits: 2,
                         maximumFractionDigits: 2}) }</p>
                         <p key={pmt.transactionNumber + 'principalPayment'}>Principal Payment: ${ pmt.principalPmt.toLocaleString(undefined,{minimumFractionDigits: 2,
                         maximumFractionDigits: 2})  }</p>
                         <p key={pmt.transactionNumber + 'interestPayment'}>Interest Payment: ${ pmt.intPmt.toLocaleString(undefined,{minimumFractionDigits: 2,
                         maximumFractionDigits: 2})  }</p>
-                        <p key={pmt.transactionNumber + 'currentPayment'}>Total Current Payment: ${ pmt.currentPayment.toLocaleString(undefined,{minimumFractionDigits: 2,
+                        <p key={pmt.transactionNumber + 'currentPayment'}>Total Current Payment: ${ pmt.payment.toLocaleString(undefined,{minimumFractionDigits: 2,
                         maximumFractionDigits: 2})  }</p>
                         <p key={pmt.transactionNumber + 'principal'}>New Principal: ${pmt.principal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}</p>
-                        <p key={pmt.transactionNumber + 'totalInterestOwed'}>Total Interest Owed: ${pmt.totalInterestOwed.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}</p>
+                        <p key={pmt.transactionNumber + 'interestOwed'}>Total Interest Owed: ${pmt.interestOwed.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}</p>
                         <p key={pmt.transactionNumber + 'newBalance'}>New Balance: ${ pmt.newBalance.toLocaleString(undefined,{minimumFractionDigits: 2,
                         maximumFractionDigits: 2})  }</p>
                         {pmt.remainingPayments > 0
